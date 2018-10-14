3bfa123316bf5bd2831e08409f978cf6ec71c219
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-14 19:38:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,4 +10,13 @@ function forLoop(array) {
   return array
 }
 
-function whileLoop(n) {}
+function whileLoop(n) {
+  let countdown = n {
+    while (countdown>0) {
+      console.log(--countdown)
+    }
+    
+  }
+  return ("done")  
+}
+
